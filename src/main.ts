b6f369cc1eb01ffd2bf83df11b6d845b194cc6af
adius0711 <athota.adi.us@gmail.com>
2024-10-31 16:41:57 -0500

    users component

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { HeaderComponent } from './app/header/header.component';
+import { UserComponent } from './app/user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   template: `
-    <app-header></app-header>
+    <app-header />
+    <main>
+    <ul id="users">
+      <li>
+      <app-user />
+      </li>
+    </ul>
+    </main>
   `,
 })
 export class App {
