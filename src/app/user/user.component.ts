37fab6cbacb8caf7f08ec1bcbecc1cd16dcd97ce
adius0711 <athota.adi.us@gmail.com>
2024-10-31 18:58:55 -0500

    add interfaces and make code prettier

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,58 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../../dummy-users';
+import {
+  Component,
+  computed,
+  signal,
+  Input,
+  input,
+  Output,
+  EventEmitter,
+  output,
+} from "@angular/core";
+// import { DUMMY_USERS } from '../../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+type User = {
+  id: string;
+  avatar: string;
+  name: string;
+};
 
 @Component({
-  selector: 'app-user',
+  selector: "app-user",
   standalone: true,
   imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  templateUrl: "./user.component.html",
+  styleUrl: "./user.component.css",
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
 
+  @Input({ required: true }) user!: User;
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // @Output() select = new EventEmitter<string>();
+
+  select = output<string>();
+
+  get imagePath() {
+    return "assets/" + this.user.avatar;
+  }
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/' + this.avatar())
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser().avatar
+  // }
+
+  onSelectUser() {
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // this.selectedUser = DUMMY_USERS[randomIndex]
+    this.select.emit(this.user.id);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,28 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { HeaderComponent } from './app/header/header.component';
-import { UserComponent } from './app/user/user.component';
+import { Component } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { HeaderComponent } from "./app/header/header.component";
+import { UserComponent } from "./app/user/user.component";
+import { TasksComponent } from "./app/tasks/tasks.component";
+import { DUMMY_USERS } from "./dummy-users";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
-  template: `
-    <app-header />
-    <main>
-    <ul id="users">
-      <li>
-      <app-user />
-      </li>
-    </ul>
-    </main>
-  `,
+  imports: [HeaderComponent, UserComponent, TasksComponent],
+  templateUrl: "./app.component.html",
 })
 export class App {
-  name = 'Angular';
+  name = "Angular";
+  users = DUMMY_USERS;
+  selectedUserId = "u1";
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
 
 bootstrapApplication(App);
